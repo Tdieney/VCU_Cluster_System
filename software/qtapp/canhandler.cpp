4a4129caa7565c267ff28f5958f18703519c170e
Tdieney <vanthinh.nguyentran.4@gmail.com>
2025-07-11 03:23:09 +0700

    Multithreading for CAN Rx/Tx handler

--- software/qtapp/canhandler.cpp
+++ software/qtapp/canhandler.cpp
@@ -3,71 +3,165 @@
 #include <linux/can/raw.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <sys/types.h>
 #include <net/if.h>
 #include <unistd.h>
 #include <cstring>
 #include <QDebug>
 
+// Transmit thread
+class CanTxThread : public QThread {
+    Q_OBJECT
+public:
+    CanTxThread(QObject *parent = nullptr)
+        : QThread(parent), m_socket(-1), m_running(false) {}
+
+    ~CanTxThread() { stop(); }
+
+    void enqueueMessage(const struct can_frame &frame) {
+        QMutexLocker locker(&m_mutex);
+        m_queue.enqueue(frame);
+    }
+
+    void stop() {
+        m_running = false;
+        wait();
+        if (m_socket >= 0) close(m_socket);
+    }
+
+protected:
+    void run() override {
+        struct sockaddr_can addr;
+        struct ifreq ifr;
+
+        m_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+        if (m_socket < 0) {
+            qWarning() << "TX: Error opening CAN socket";
+            return;
+        }
+        strcpy(ifr.ifr_name, "can0");
+        if (ioctl(m_socket, SIOCGIFINDEX, &ifr) < 0) {
+            qWarning() << "TX: Fail to specify CAN interface";
+            return;
+        }
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+        if (bind(m_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+            qWarning() << "TX: Error binding CAN socket";
+            close(m_socket);
+            return;
+        }
+
+        m_running = true;
+        while (m_running) {
+            m_mutex.lock();
+            if (!m_queue.isEmpty()) {
+                struct can_frame frame = m_queue.dequeue();
+                m_mutex.unlock();
+                int nbytes = write(m_socket, &frame, sizeof(frame));
+                if (nbytes < 0) {
+                    qWarning() << "TX: Error writing CAN frame";
+                }
+            } else {
+                m_mutex.unlock();
+                msleep(10);
+            }
+        }
+        close(m_socket);
+    }
+
+private:
+    int m_socket;
+    bool m_running;
+    QMutex m_mutex;
+    QQueue<struct can_frame> m_queue;
+};
+
+// Receive thread
+class CanRxThread : public QThread {
+    Q_OBJECT
+public:
+    CanRxThread(QObject *parent = nullptr)
+        : QThread(parent), m_socket(-1), m_running(false) {}
+
+    ~CanRxThread() { stop(); }
+
+    void stop() {
+        m_running = false;
+        wait();
+        if (m_socket >= 0) close(m_socket);
+    }
+
+signals:
+    void speedChanged(int speed);
+    void batteryChanged(int battery);
+
+protected:
+    void run() override {
+        struct sockaddr_can addr;
+        struct ifreq ifr;
+        struct can_frame rx_frame;
+
+        m_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+        if (m_socket < 0) {
+            qWarning() << "RX: Error opening CAN socket";
+            return;
+        }
+        strcpy(ifr.ifr_name, "can0");
+        if (ioctl(m_socket, SIOCGIFINDEX, &ifr) < 0) {
+            qWarning() << "RX: Fail to specify CAN interface";
+            return;
+        }
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+        if (bind(m_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+            qWarning() << "RX: Error binding CAN socket";
+            close(m_socket);
+            return;
+        }
+
+        m_running = true;
+        while (m_running) {
+            int nbytes = read(m_socket, &rx_frame, sizeof(struct can_frame));
+            if (nbytes > 0) {
+                if (rx_frame.can_id == 0x100) {
+                    emit speedChanged(rx_frame.data[0]);
+                }
+                if (rx_frame.can_id == 0x101) {
+                    emit batteryChanged(rx_frame.data[0]);
+                }
+            }
+        }
+        close(m_socket);
+    }
+
+private:
+    int m_socket;
+    bool m_running;
+};
+
+// CanHandler implementation
 CanHandler::CanHandler(QObject *parent)
-    : QThread(parent), m_socket(-1), m_running(false) {
-    // Set CAN parameters
+    : QObject(parent)
+{
+    // Set CAN parameters and bring up interface
     system(IP_CMD_SET_CAN0_PARAMS);
-    // Bring up the CAN interface
     system(IP_CMD_UP_CAN0);
-    qDebug() << "CAN interface set up and brought up.";
+
+    m_txThread = new CanTxThread(this);
+    m_rxThread = new CanRxThread(this);
+
+    connect(m_rxThread, &CanRxThread::speedChanged, this, &CanHandler::speedChanged);
+    connect(m_rxThread, &CanRxThread::batteryChanged, this, &CanHandler::batteryChanged);
+
+    m_txThread->start();
+    m_rxThread->start();
 }
 
 CanHandler::~CanHandler() {
-    stop();
+    m_txThread->stop();
+    m_rxThread->stop();
 }
 
-void CanHandler::run() {
-    struct sockaddr_can addr;
-    struct ifreq ifr;
-    struct can_frame frame;
-
-    m_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    if (m_socket < 0) {
-        qWarning() << "Error opening CAN socket";
-        return;
-    }
-
-    strcpy(ifr.ifr_name, "can0");
-    ioctl(m_socket, SIOCGIFINDEX, &ifr);
-
-    addr.can_family = AF_CAN;
-    addr.can_ifindex = ifr.ifr_ifindex;
-
-    if (bind(m_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        qWarning() << "Error binding CAN socket";
-        close(m_socket);
-        return;
-    }
-
-    m_running = true;
-    while (m_running) {
-        int nbytes = read(m_socket, &frame, sizeof(struct can_frame));
-        if (nbytes > 0) {
-            // Example: parse speed and battery from CAN frame
-            if (frame.can_id == 0x100) {
-                int speed = frame.data[0];
-                emit speedChanged(speed);
-            }
-            if (frame.can_id == 0x101) {
-                int battery = frame.data[0];
-                emit batteryChanged(battery);
-            }
-        }
-    }
-    close(m_socket);
-}
-
-void CanHandler::stop() {
-    m_running = false;
-    if (m_socket >= 0)
-    {
-        close(m_socket);
-        system(IP_CMD_DOWN_CAN0);
-    }
+void CanHandler::sendMessage(const struct can_frame &frame) {
+    m_txThread->enqueueMessage(frame);
 }
--- software/qtapp/canhandler.h
+++ software/qtapp/canhandler.h
@@ -4,26 +4,32 @@
 #include <QObject>
 #include <QThread>
 #include <QString>
+#include <QMutex>
+#include <QQueue>
+#include <linux/can.h>
 
 #define IP_CMD_SET_CAN0_PARAMS  "ip link set can0 type can bitrate 666666"
 #define IP_CMD_UP_CAN0          "ip link set can0 up"
 #define IP_CMD_DOWN_CAN0        "ip link set can0 down"
 
-class CanHandler : public QThread {
+class CanTxThread;
+class CanRxThread;
+
+class CanHandler : public QObject {
     Q_OBJECT
 public:
     explicit CanHandler(QObject *parent = nullptr);
     ~CanHandler();
-    void run() override;
-    void stop();
+
+    void sendMessage(const struct can_frame &frame);
 
 signals:
     void speedChanged(int speed);
     void batteryChanged(int battery);
 
 private:
-    int m_socket;
-    bool m_running;
+    CanTxThread *m_txThread;
+    CanRxThread *m_rxThread;
 };
 
 #endif // CANHANDLER_H
